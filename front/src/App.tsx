f7a690628845670bdbf775ff4b9dcbabbd24cee3
Bill <pakawatsan61@gmail.com>
2025-04-02 11:04:59 +0700

    feat: add Navbar component and update dependencies

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,22 +3,31 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button, HStack, Avatar } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import Navbar from "./components/main/nav";
 
 function App() {
-	const Demo = () => {
-		return (
-			<HStack>
-				<Button>Click me</Button>
-				<Button>Click me</Button>
-				<Avatar.Root>
-					<Avatar.Fallback name="Segun Adebayo" />
-					<Avatar.Image src="https://bit.ly/sage-adebayo" />
-				</Avatar.Root>
-			</HStack>
-		);
-	};
 
-	return Demo();
+	return (
+		<>
+			<Navbar />
+			<Box
+				display="flex"
+				justifyContent="center"
+				alignItems="center"
+				height="100vh"
+				backgroundColor="gray.100"
+				padding="20px"
+				className="transition-all duration-200">
+				<div>
+					<a href="https://vitejs.dev" target="_blank">
+						<img src={viteLogo} className="logo" alt="Vite logo" />
+					</a>
+				</div>
+				
+				</Box>
+		</>
+	);
 }
 
 export default App;
